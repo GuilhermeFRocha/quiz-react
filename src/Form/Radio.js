653f30eb83942f5765434c20688f8ab1ac99e7a5
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-02-23 20:10:27 -0300

    estilizando

--- src/Form/Radio.js
+++ src/Form/Radio.js
@@ -4,10 +4,10 @@ const radio = ({pergunta, options, id,value, onChange, active}) => {
 
   if(active === false) return null;
   return (
-    <fieldset style={{padding:'2rem', marginBottom:'1rem', border:'2px solid #eee'}}>
-      <legend style={{fontWeight:'bold'}}>{pergunta}</legend>
+    <fieldset style={{padding:'2rem', marginBottom:'1rem', border:'2px solid rgb(17 153 255)', backdropFilter:'blur(10px)', background:'rgba(0,0,0,0.1)'}}>
+      <legend style={{fontWeight:'bold', color:'#ef657f'}}>{pergunta}</legend>
       {options.map((option) => (
-      <label key={option}  style={{marginBottom:'1rem', fontFamily:'monospace'}}>
+      <label key={option}  style={{cursor:'pointer',marginBottom:'1rem', fontFamily:'monospace'}}>
         <input 
         type="radio"
         id={id}
